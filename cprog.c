4cec1813067d94e148a7b2d058c5c1a533d29718
root <maheshlagali@gmail.com>
2020-02-09 14:33:26 +0000

    I have added a new line to cprog.c file to check branch

--- cprog.c
+++ cprog.c
@@ -3,7 +3,9 @@ int main()
 { 
    /* printf function displays the content that is
     * passed between the double quotes.
-    */
+    *
+    * this line i have added in branch develop	
+   /
    printf("Hello World");
    return 0;
 }
